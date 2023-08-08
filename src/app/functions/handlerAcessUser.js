083372bedd854400f8c636cec6f8a3399f5195e5
stefy2906 <stefanybreve@gmail.com>
2023-08-08 17:34:45 -0400

    commit

--- src/app/functions/handlerAcessUser.js
+++ src/app/functions/handlerAcessUser.js
@@ -1,7 +1,14 @@
 import Cookies from "js-cookie";
 
 const handlerAcessUser = () => {
-     Cookies.set('token','Este é um token!');
+    const responseIsUserAtuh={
+        name: 'Stefany',
+        email:'stefany@gmail.com',
+        token: 'token'
+    };
+    if(responseIsUserAtuh.token==="token"){
+        Cookies.set('token',responseIsUserAtuh.token);
+    }
 }
 
-export {handlerAcessUser}
+export  { handlerAcessUser };
